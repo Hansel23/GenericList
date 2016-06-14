05433def9d4163805017c8bf46a3b9993328c7fb
Benjamin Bruska <benjamin.bruska@fortuneglobe.com>
2016-06-14 16:35:19 +0200

    add constants for scalar types, to avoid using current typenames of php, which may change in future

--- src/GenericList.php
+++ src/GenericList.php
@@ -2,7 +2,6 @@
 namespace Hansel23\GenericLists;
 
 use Hansel23\GenericLists\Exceptions\InvalidTypeException;
-use Hansel23\GenericLists\Interfaces\ListsItems;
 
 /**
  * Class GenericList
@@ -11,19 +10,31 @@ use Hansel23\GenericLists\Interfaces\ListsItems;
  */
 class GenericList extends AbstractList
 {
-	const OBJECT  = 1;
+	private static $OBJECT_TYPE  = 1;
 
-	const STRING  = 2;
+	private static $STRING_TYPE  = 2;
 
-	const INT     = 3;
+	private static $INT_TYPE     = 3;
 
-	const FLOAT   = 4;
+	private static $FLOAT_TYPE   = 4;
 
-	const ARR     = 5;
+	private static $ARRAY_TYPE   = 5;
 
-	const BOOL    = 6;
+	private static $BOOL_TYPE    = 6;
 
-	const NUMERIC = 7;
+	private static $NUMERIC_TYPE = 7;
+
+	const STRING  = 'string';
+
+	const INT     = 'integer';
+
+	const FLOAT   = 'float';
+
+	const _ARRAY  = 'array';
+
+	const BOOL    = 'boolean';
+
+	const NUMERIC = 'numeric';
 
 	/**
 	 * @var string
@@ -39,6 +50,8 @@ class GenericList extends AbstractList
 	 * GenericList constructor.
 	 *
 	 * @param $itemTypeName
+	 * 
+	 * @throws InvalidTypeException
 	 */
 	public function __construct( $itemTypeName )
 	{
@@ -46,35 +59,39 @@ class GenericList extends AbstractList
 
 		if ( class_exists( $itemTypeName ) || interface_exists( $itemTypeName ) )
 		{
-			$this->itemType = self::OBJECT;
-		}
-		elseif ( $itemTypeName == 'string' )
-		{
-			$this->itemType = self::STRING;
-		}
-		elseif ( $itemTypeName == 'integer' )
-		{
-			$this->itemType = self::INT;
-		}
-		elseif ( $itemTypeName == 'array' )
-		{
-			$this->itemType = self::ARR;
-		}
-		elseif ( $itemTypeName == 'double' )
-		{
-			$this->itemType = self::FLOAT;
-		}
-		elseif ( $itemTypeName == 'boolean' )
-		{
-			$this->itemType = self::BOOL;
-		}
-		elseif ( $itemTypeName == 'numeric' )
-		{
-			$this->itemType = self::NUMERIC;
+			$this->itemType = self::$OBJECT_TYPE;
 		}
 		else
 		{
-			throw new InvalidTypeException( sprintf( 'Type "%s" not allowed', $itemTypeName ) );
+			switch ( $itemTypeName )
+			{
+				case self::STRING:
+					$this->itemType = self::$STRING_TYPE;
+					break;
+
+				case self::INT:
+					$this->itemType = self::$INT_TYPE;
+					break;
+
+				case self::_ARRAY:
+					$this->itemType = self::$ARRAY_TYPE;
+					break;
+
+				case self::FLOAT:
+					$this->itemType = self::$FLOAT_TYPE;
+					break;
+
+				case self::BOOL:
+					$this->itemType = self::$BOOL_TYPE;
+					break;
+
+				case self::NUMERIC:
+					$this->itemType = self::$NUMERIC_TYPE;
+					break;
+
+				default:
+					throw new InvalidTypeException( sprintf( 'Type "%s" not allowed', $itemTypeName ) );
+			}
 		}
 	}
 
@@ -95,41 +112,31 @@ class GenericList extends AbstractList
 	{
 		switch ( $this->itemType )
 		{
-			case self::OBJECT:
+			case self::$OBJECT_TYPE:
 				return $item instanceof $this->validItemTypeName;
 
-			case self::STRING:
+			case self::$STRING_TYPE:
 				return is_string( $item );
 
-			case self::INT:
+			case self::$INT_TYPE:
 				return is_int( $item );
 
-			case self::ARR:
+			case self::$ARRAY_TYPE:
 				return is_array( $item );
 
-			case self::FLOAT:
+			case self::$FLOAT_TYPE:
 				return is_float( $item );
 
-			case self::BOOL:
+			case self::$BOOL_TYPE:
 				return is_bool( $item );
 
-			case self::NUMERIC:
+			case self::$NUMERIC_TYPE:
 				return is_numeric( $item );
+			
+			// @codeCoverageIgnoreStart
+			default:
+				return false;
 		}
-
-		return false;
-	}
-
-	/**
-	 * @param ListsItems $list
-	 *
-	 * @return bool
-	 */
-	protected function isValidList( ListsItems $list )
-	{
-		$validItemType = $this->getItemType();
-		$itemType      = $list->getItemType();
-
-		return $itemType == $validItemType || in_array( $validItemType, class_implements( $itemType ) );
+		// @codeCoverageIgnoreEnd
 	}
 }
